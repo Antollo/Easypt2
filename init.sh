b5c2e77d52cfa4e8d316d7fd9f20225b7b654550
Antoni Nowinowski <antoninowinowski@hotmail.com>
2023-01-13 17:16:03 +0100

    Apply suggestions from #9

--- init.sh
+++ init.sh
@@ -1,7 +1,15 @@
-sudo apt install -y flex unzip
+sudo apt install -y flex unzip libssl-dev
 
-wget -O /tmp/llnextgen.deb "https://os.ghalkes.nl/LLnextgen/releases/llnextgen_0.5.5-1_amd64.deb"
-sudo apt install -y /tmp/llnextgen.deb
+if [ "$(arch)" = "x86_64" ] ; then
+    wget -O /tmp/llnextgen.deb "https://os.ghalkes.nl/LLnextgen/releases/llnextgen_0.5.5-1_amd64.deb"
+    sudo apt install -y /tmp/llnextgen.deb
+elif [ "$(arch)" = "i386" ] || [ "$(arch)" = "i686" ] ; then
+    wget -O /tmp/llnextgen.deb "https://os.ghalkes.nl/LLnextgen/releases/llnextgen_0.5.5-1_i386.deb"
+    sudo apt install -y /tmp/llnextgen.deb
+else
+    echo "## No precompiled binaries for LLnextgen available."
+    echo "## Visit: https://os.ghalkes.nl/LLnextgen/ to compile and install LLnextgen!"
+fi
 
 wget -O /tmp/glfw.zip "https://github.com/glfw/glfw/releases/download/3.3.6/glfw-3.3.6.zip"
 rm -r /tmp/glfw
